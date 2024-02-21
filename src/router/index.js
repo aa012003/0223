832895f23867c6be5b204e319adc584723dad6d5
joy <aa012003@gmail.com>
2024-02-22 00:07:09 +0800

    week6

---
+++ src/router/index.js
@@ -0,0 +1,60 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('../views/FrontView.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Home',
+        component: () => import('../views/HomeView.vue'),
+      },
+      {
+        path: 'about',
+        name: 'About',
+        component: () => import('../views/AboutView.vue'),
+      },
+      {
+        path: 'cart',
+        name: 'Cart',
+        component: () => import('../views/CartView.vue'),
+      },
+      {
+        path: 'products',
+        name: 'Products',
+        component: () => import('../views/ProductsView.vue'),
+      },
+    ],
+  },
+  {
+    path: '/login',
+    component: () => import('../views/LoginView.vue'),
+  },
+  {
+    path: '/admin',
+    component: () => import('../views/dashboard/DashboardView.vue'),
+    children: [
+      {
+        path: 'order',
+        component: () => import('../views/dashboard/OrderView.vue'),
+      },
+      {
+        path: 'product',
+        component: () => import('../views/dashboard/ProductView.vue'),
+      },
+      {
+        path: 'login',
+        component: () => import('../views/dashboard/LoginView.vue'),
+      },
+    ],
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  linkActiveClass: 'active',
+  routes,
+});
+
+export default router;
---
+++ src/views/CartView.vue
@@ -0,0 +1,4 @@
+<template>
+  <h2>這是購物車列表</h2>
+  <RouterView> </RouterView>
+</template>
