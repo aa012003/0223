da35aabb44ebf75bcf44ce60ad0f1debe4c5dfbd
joy <aa012003@gmail.com>
2024-02-28 00:31:56 +0800

    第六周補教

--- src/views/front/ProductsView.vue
+++ src/views/front/ProductsView.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable camelcase -->
 <template>
   <user-Modal :temp-product="tempProduct" :add-cart="addCart" ref="userModal"></user-Modal>
 
@@ -43,100 +44,13 @@
       </table>
     </div>
   </div>
-
-  <div type="text/x-template" id="userProductModal">
-    <div
-      class="modal fade"
-      id="productModal"
-      tabindex="-1"
-      role="dialog"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
-      ref="modal"
-    >
-      <div class="modal-dialog modal-xl" role="document">
-        <div class="modal-content border-0">
-          <div class="modal-header bg-dark text-white">
-            <h5 class="modal-title" id="exampleModalLabel">
-              <span>{{}}</span>
-            </h5>
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div class="modal-body">
-            <div class="row">
-              <div class="col-sm-6">
-                <!-- <img class="img-fluid" :src="" alt=""> -->
-              </div>
-              <div class="col-sm-6">
-                <span class="badge bg-primary rounded-pill">{{}}</span>
-                <p>商品描述：{{}}</p>
-                <p>商品內容：{{}}</p>
-                <div class="h5">{{}} 元</div>
-                <del class="h6">原價 {{}} 元</del>
-                <div class="h5">現在只要 {{}} 元</div>
-                <div>
-                  <div class="input-group">
-                    <input type="number" class="form-control" min="1" />
-                    <button type="button" class="btn btn-primary">加入購物車</button>
-                  </div>
-                </div>
-              </div>
-              <!-- col-sm-6 end -->
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
 </template>
+
 <script>
 import axios from 'axios';
-import { Modal } from 'bootstrap';
+import userModal from '../../components/UserModal.vue';
 
 const { VITE_URL, VITE_PATH } = import.meta.env;
-
-const userModal = {
-  props: ['tempProduct', 'addCart'],
-  data() {
-    return {
-      productModal: null,
-      qty: 1,
-    };
-  },
-  template: '#userProductModal',
-  methods: {
-    open() {
-      this.$nextTick(() => {
-        if (this.productModal) {
-          this.productModal.show();
-        }
-      });
-    },
-    close() {
-      this.$nextTick(() => {
-        if (this.productModal) {
-          this.productModal.hide();
-        }
-      });
-    },
-  },
-  watch: {
-    tempProduct() {
-      this.qty = 1;
-    },
-  },
-  mounted() {
-    this.$nextTick(() => {
-      this.productModal = new Modal(this.$refs.modal);
-      console.log('Modal initialized:', this.productModal);
-    });
-  },
-};
 
 export default {
   components: {
@@ -147,25 +61,32 @@ export default {
       products: [],
       tempProduct: {},
       carts: {},
+      status: {
+        addCartLoading: '',
+      },
     };
   },
   methods: {
-    getProducts(product) {
-      this.tempProduct = product;
-      this.$refs.userModal.open();
+    getProducts() {
+      axios.get(`${VITE_URL}/api/${VITE_PATH}/products/all`).then((res) => {
+        // console.log(res);
+        this.products = res.data.products;
+      });
     },
-    addCart(productId, qty = 1) {
+    // eslint-disable-next-line camelcase
+    addCart(product_id, qty = 1) {
       const order = {
-        productId,
+        // eslint-disable-next-line camelcase
+        product_id,
         qty,
       };
-      this.status.addCartLoading = productId;
+      // eslint-disable-next-line camelcase
+      this.status.addCartLoading = product_id;
       axios.post(`${VITE_URL}/api/${VITE_PATH}/cart`, { data: order }).then((res) => {
         console.log(res);
         alert(res.data.message);
         this.status.addCartLoading = '';
         this.$refs.userModal.close();
-        this.getCart();
       });
     },
     openModal(product) {
